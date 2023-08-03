58fb7cfc881c4c079cd1af635e9f4d05eddda9f6
Jonathan Bijl <sannoayo@gmail.com>
2023-08-02 21:32:55 -0400

    updated signuppage

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -20,7 +20,23 @@ function SignupPage(props) {
   const handleName = (e) => setName(e.target.value);
 
   
-  const handleSignupSubmit = (e) => {};
+  const handleSignupSubmit = (e) => {
+    e.preventDefault();
+    // Create an object representing the request body
+    const requestBody = { email, password, name };
+ 
+    // Make an axios request to the API
+    // If the POST request is a successful redirect to the login page
+    // If the request resolves with an error, set the error message in the state
+    axios.post(`${API_URL}/auth/signup`, requestBody)
+      .then((response) => {
+        navigate('/login');
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      })
+  };
 
   
   return (
